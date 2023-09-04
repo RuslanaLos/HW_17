e198cd75c2819b0343f19f968f27a731df87d63b
RuslanaLos <ruslanalos74@gmail.com>
2023-09-04 07:14:19 +0300

    hw_17

--- js/app4.js
+++ js/app4.js
@@ -1,5 +1,5 @@
-const imageByRandom = document.getElementById('randomImage');
 const buttonOfChange = document.getElementById('changeImage');
+const containerForImage = document.querySelector('.image_container');
 const amountOfImages = 9;
 function getNumberOfImg() {
     return Math.floor(Math.random() * amountOfImages) + 1;
@@ -7,7 +7,11 @@ function getNumberOfImg() {
 
 function changeRandomImage() {
     let randomNum = getNumberOfImg();
-    imageByRandom.src  = `images/${randomNum}.jpeg`;
+    const imgForUser = document.createElement('img');
+    imgForUser.src  = `images/${randomNum}.jpeg`;
+
+    containerForImage.innerHTML = '';
+    containerForImage.appendChild(imgForUser);
 }
 buttonOfChange.addEventListener('click', changeRandomImage);
 changeRandomImage();
